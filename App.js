370133cd84e9b160536bdfa8545f81d49a46b63f
Farid Safi <FaridSafi@users.noreply.github.com>
2019-11-29 10:03:22 +0100

    4

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import SignIn from "./containers/SignIn";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js !</Text>
+      <SignIn />
     </View>
   );
 }
