f29dfd8ff0b46ccedaeafa863faefeb7950ac379
Farid Safi <FaridSafi@users.noreply.github.com>
2019-11-29 09:56:43 +0100

    3

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js</Text>
+      <Text>Open up App.js !</Text>
     </View>
   );
 }
